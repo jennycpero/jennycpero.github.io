28e0f9cc8830cd225c16b5af56898dd353a92744
jennycpero <jennycpero@gmail.com>
2023-07-08 12:19:51 -0500

    Created multi text transition for hero page

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-/* Alternate hero header text between Data Analytics, Technical Writing, and UX Writing. */
+/* Alternate hero header text between Data Analytics,  Technical Writing, Data Visaulization, and UX Writing. */
+const heroTexts = ["Data Analytics", "Technical Writing", "UX Writing"];
+let counter = 0;
+
+setInterval(change, 3500);
+
+function change() {
+  document.getElementById("hero-header").setAttribute("class", "text-fade"); 
+
+  setTimeout(() => {
+    document.getElementById("hero-header").textContent = heroTexts[counter];
+    document.getElementById("hero-header").setAttribute("class", "text-show");
+  }, 500)
+
+  counter++;
+
+  if (counter >= heroTexts.length) {
+    counter = 0;
+  }
+}
